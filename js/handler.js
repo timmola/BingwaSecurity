fd74ad3007d0147c4a006498369ad4463197e70a
tatsv <75327515+timmola@users.noreply.github.com>
2024-03-11 03:36:43 +0300

    Update handler.js

--- js/handler.js
+++ js/handler.js
@@ -1,4 +1,4 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+document.getElementById("contact_form").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent the form from submitting normally
 
   // Get form values
