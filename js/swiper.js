1d5a0b5e59b1fcd1741e477a3b96ad3ad8fbb163
tatsv <75327515+timmola@users.noreply.github.com>
2024-03-25 10:59:35 +0300

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,12 @@
 
+    // Initialize Swiper
+    var swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      loop: true,
+      autoplay: {
+        delay: 5000, // Auto play interval in milliseconds
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
